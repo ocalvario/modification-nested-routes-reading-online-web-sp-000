c09133ca27eb58fa851b70171cbeccbb1792beac
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-21 22:06:13 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
 
+  def new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+  end
+end
+  
   def index
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
@@ -31,9 +39,19 @@ class PostsController < ApplicationController
     @post.update(post_params)
     redirect_to post_path(@post)
   end
-
+    
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
@@ -41,4 +59,10 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :description)
   end
+end
+
+private
+ 
+def post_params
+  params.require(:post).permit(:title, :description, :author_id)
 end

f8d25e1de46fdfc001b8ad4ab7604096caa84846
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-21 22:05:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
 
   def new
-    @post = Post.new(author_id: params[:author_id])
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
   end
+end
   
   def index
     if params[:author_id]
